26b4255457d3a04bae5e899e1870beac86e814a3
Quantum <quantum0@mail.ru>
2018-10-31 04:21:54 +0300

    Add files via upload

---
+++ Server Flask Code.py
@@ -0,0 +1,33 @@
+﻿from flask_cors import cross_origin
+
+# v1 :
+
+@app.route('/sendfalink/<frm>/<id>', methods=['GET'])
+@cross_origin()
+def sfl(frm, id):
+    if id != 0:
+        data = dict(access_token = q0not, v = 5.73, peer_id = 2000000002, message = '== Meow ==\nПользователь' + frm.title() + ' поделился артом с ФА: http://www.furaffinity.net/view/' + str(id))
+        resp = requests.post('https://api.vk.com/method/messages.send?', data).json()
+        if 'response' in resp:
+            return 'yay'
+        else:
+            telelog(resp)
+    return 'no :c'
+	
+	
+# v2 :
+
+@app.route('/sendfalink/<id>', methods=['POST'])
+@cross_origin()
+def sfl(id):
+    Json = request.get_json()
+    telelog(Json)
+    frm = Json['from']
+    if id != 0:
+        data = dict(access_token = q0not, v = 5.73, peer_id = 2000000002, message = '== Meow ==\nПользователь ' + frm.title() + ' поделился артом с ФА:\n"' + Json['title'] + '" от ' + Json['artist'] + '\nhttp://www.furaffinity.net/view/' + str(id))
+        resp = requests.post('https://api.vk.com/method/messages.send?', data).json()
+        if 'response' in resp:
+            return 'yay'
+        else:
+            telelog(resp)
+    return 'no :c'
---
+++ fa_inject.js
@@ -0,0 +1,37 @@
+(function (){
+	var elements = document.getElementsByClassName("alt1 actions aligncenter");
+	
+	var st = document.createElement('b');
+	// KKP - name of dialog in Vk
+    st.innerHTML = ' | <a href="#">Share in KKP</a> &nbsp';
+    st.className = 'sendKKP';
+	st.onclick=function(e){
+		e.preventDefault();
+		var me = document.getElementById('my-username').innerText.substr(1);
+		var title = document.getElementsByClassName("cat")[0].innerText;
+		var artist = document.getElementsByClassName("cat")[2].getElementsByTagName('a')[0].innerText;
+		var url = document.location.href.slice(0, -1).split('/')[4];
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function()
+			{ 
+			if (xmlHttp.readyState == 4 && xmlHttp.status == 200 && xmlHttp.responseText == 'yay')
+				document.getElementsByClassName("sendKKP")[0].innerHTML = ' | <a href="#" onclick="">Sent ^-^</a> &nbsp'
+			else
+				document.getElementsByClassName("sendKKP")[0].innerHTML = ' | <a href="#" onclick="">Error :c</a> &nbsp'
+			}
+		xmlHttp.open("POST", "http://quantum0.pythonanywhere.com/sendfalink/" + url, true);
+		xmlHttp.setRequestHeader("Content-Type", "application/json");
+		var data = JSON.stringify({"from": me, "id": url, "artist": artist, "title": title});
+		xmlHttp.send(data);
+		document.getElementsByClassName("sendKKP")[0].innerHTML = ' | <a href="#" onclick="">Sending...</a> &nbsp'
+		return false;
+	}
+
+	for (var i = 0; i < elements.length; i++)
+	{
+		var bs = elements[0].getElementsByTagName('b');
+		bs[3].parentNode.insertBefore(st, bs[3].nextSibling);
+	}
+	
+})();
+
